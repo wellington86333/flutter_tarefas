b215ac6ba789dc13bf1470e0cf70d6344ab9b027
Wellington Brito <wellington.brito@rocketmail.com>
2022-12-18 23:01:42 -0300

    Incrementando a lsita de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -21,20 +21,26 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Task("Aprender Flutter no café da manhã comendo sucrilhos"),
-            Task("Andar de Bike"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
-            Task("Meditar"),
+            Task(
+                "Aprender Flutter ",
+                "https://cdn-images-1.medium.com/max/406/1*YkWeEU6ii0k_DW0bmrD0Ig.png",
+                3),
+            Task(
+                "Andar de Bike",
+                "http://pedaladasaudavel.com.br/wp-content/uploads/2019/01/blog-cicles-jaime-dicas-para-pedalar-a-noite2-e1547063954595-840x480.jpg",
+                2),
+            Task(
+                "Meditar",
+                "https://magscan.com.br/wp-content/uploads/2020/04/original-2ce8ad3de83875daa42bae2bc572c235.jpeg",
+                5),
+            Task(
+                "Ler",
+                "https://cdn.atenaeditora.com.br/documentos/blog_imagem/35/conversions/1-3-optimized.jpg",
+                4),
+            Task(
+                "Jogar",
+                "https://s3-sa-east-1.amazonaws.com/uploads-anchieta-br/wp-content/uploads/sites/7/2019/03/18144919/foto-como-jogar-video-game-1.jpg",
+                1),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -45,7 +51,9 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome;
-  const Task(this.nome, {super.key});
+  final String foto;
+  final int dificuldade;
+  const Task(this.nome, this.foto, this.dificuldade, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -61,30 +69,92 @@ class _TaskState extends State<Task> {
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
+              ),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.black26,
+                        ),
                         width: 72,
                         height: 100,
-                        child: Image.network(src),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(
-                            widget.nome,
-                            style: TextStyle(
-                                fontSize: 24, overflow: TextOverflow.ellipsis),
-                          )),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                       Container(
                         width: 52,
                         height: 52,
@@ -116,11 +186,14 @@ class _TaskState extends State<Task> {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
-                          width: 200,
-                          child: LinearProgressIndicator(
-                            color: Colors.white,
-                            value: nivel / 10,
-                          )),
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade) / 10
+                              : 1,
+                        ),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(12.0),
